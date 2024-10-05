edcabe2fc130955d32f6dc496117fac51d20e9a6
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2024-10-06 02:46:59 +0700

    start sub product

---
+++ src/constants/listColors.ts
@@ -0,0 +1,13 @@
+export const listColors = [
+  "magenta",
+  "red",
+  "volcano",
+  "orange",
+  "gold",
+  "lime",
+  "green",
+  "cyan",
+  "blue",
+  "geekblue",
+  "purple",
+];
--- src/screens/admin/inventories/InventoryScreen.tsx
+++ src/screens/admin/inventories/InventoryScreen.tsx
@@ -1,18 +1,22 @@
-import { Button, Table } from 'antd'
-import React, { useEffect, useState } from 'react'
-import { ProductModel } from '../../../models/Products';
-import handleAPI from '../../../apis/handleAPI';
-import { API } from '../../../configurations/configurations';
+import { Avatar, Button, Image, Space, Table, Tag, Tooltip } from "antd";
+import React, { useEffect, useState } from "react";
+import { CategoryModel, ProductModel } from "../../../models/Products";
+import handleAPI from "../../../apis/handleAPI";
+import { API, colors } from "../../../configurations/configurations";
+import { ColumnProps } from "antd/es/table";
+import { Link } from "react-router-dom";
+import { listColors } from "../../../constants/listColors";
+import { MdLibraryAdd } from "react-icons/md";
 
 const InventoryScreen = () => {
-  const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isInitLoading, setIsInitLoading] = useState<boolean>(false);
   const [products, setProducts] = useState<ProductModel[]>([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     getProducts();
-  }, [])
-  const getProducts =async () =>{
+  }, []);
+  const getProducts = async () => {
     setIsLoading(true);
     try {
       const res = await handleAPI(`${API.PRODUCTS}/data`);
@@ -20,23 +24,87 @@ const InventoryScreen = () => {
       setProducts(res.data.result);
     } catch (error) {
       console.log(error);
-    }finally{
+    } finally {
       setIsLoading(false);
     }
   };
 
-  
+  const columns: ColumnProps<ProductModel>[] = [
+    {
+      key: "title",
+      dataIndex: "title",
+      title: "Title",
+    },
+    {
+      key: "description",
+      dataIndex: "description",
+      title: "Description",
+    },
+    {
+      key: "categories",
+      dataIndex: "categories",
+      title: "Categories",
+      render: (categories: CategoryModel[]) =>
+        categories &&
+        categories.length > 0 && (
+          <Space>
+            {categories.map((category: CategoryModel, _index) => (
+              <Link to={"/categories/detail/${item.slug}?id=${item.key}"}>
+                <Tag
+                  color={
+                    listColors[Math.floor(Math.random() * listColors.length)]
+                  }
+                >
+                  {category.name}
+                </Tag>
+              </Link>
+            ))}
+          </Space>
+        ),
+    },
+    {
+      key: "images",
+      dataIndex: "images",
+      title: "Image",
+      render: (images: string[]) =>
+        images &&
+        images.length > 0 && (
+          <Space>
+            <Avatar.Group>
+              {images.map((img, _index) => (
+                <Image src={img} width={"50px"} height={"50px"} />
+              ))}
+            </Avatar.Group>
+          </Space>
+        ),
+    },
+    {
+      key: "action",
+      dataIndex: "",
+      
+      title: "Action",
+      render: (product: ProductModel) => (
+        <Space>
+          <Tooltip title={'Add sub product'}>
+            <Button type="text">
+              <MdLibraryAdd color={colors.primary500} size={20} />
+            </Button>
+          </Tooltip>
+        </Space>
+      ),
+      fixed: 'right'
+    },
+  ];
+
   return (
     <>
       <Table
-      dataSource={products}
-      columns={[]}
-      loading={isLoading}>
-      
-      </Table>
-      
+        dataSource={products}
+        columns={columns}
+        loading={isLoading}
+      ></Table>
     </>
-  )
-}
+  );
+};
 
-export default InventoryScreen
+export default InventoryScreen;
